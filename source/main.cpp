689dc98678938be27013569174965dad272c3cc9
DoNiFF <prodoniff@gmail.com>
2024-09-24 16:27:49 +0300

    'latest'

--- source/main.cpp
+++ source/main.cpp
@@ -1,16 +1,19 @@
 ﻿#include <iostream>
 #include <vector>
-#include <thread>
 
-#include "Network/HttpServer.h"
 #include "System/FileManager.h"
-#include "Network/Server.h"
+#include "Network/HttpServer.h"
 
 int main(int argc, char* argv[])
 {
     System::FileManager* domains = new System::FileManager(DOMAINS_PATH);
     int serverPort = 8080;
 
+    std::vector<Network::HttpServer*> servers;
+    servers.push_back(new Network::HttpServer(8080, "Test\\"));
+    servers.push_back(new Network::HttpServer(8081, "Test2\\"));
+    servers[0]->start();
+    servers[1]->start();
 
     delete domains;
 }
