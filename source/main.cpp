413b54ec6086d407dc01c5959db2aeea715a097d
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-11-05 04:46:00 +0300

    Added dynamic creating new connections

--- source/main.cpp
+++ source/main.cpp
@@ -3,16 +3,21 @@
 
 #include "Debug/Log.h"
 #include "Network/WebServer.h"
+#include "System/FileManager.h"
 
 int main()
 {
     try {
         boost::asio::io_context ioContext;
+        int port = 8080;
 
-        const std::vector<std::pair<int, std::string>> projects = {
-            {8080, DOMAINS_PATH "/Test"},
-            {8081, DOMAINS_PATH "/Test2"},
-        };
+        std::vector<std::pair<int, std::string>> projects = {};
+
+        for (const auto& folder : System::FileManager::getFolders(DOMAINS_PATH))
+        {
+            projects.emplace_back(port, DOMAINS_PATH "/" + folder);
+            port++;
+        }
 
         const Network::WebServer server(ioContext, projects);
         server.start();
